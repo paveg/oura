c00cc2ab264dfcdbf7d436043df68f200b281c27
Ryota Ikezawa <pavegy@gmail.com>
2019-02-05 00:17:27 +0900

    user_information spec

---
+++ spec/oura/model/user_information_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe ::Oura::Model::UserInformation do
+  let(:argument_file) { 'spec/fixtures/user_information.json' }
+  let(:arguments) { JSON.parse(File.read(argument_file)).symbolize_keys }
+
+  describe '#initialize' do
+    subject { described_class.new(arguments) }
+
+    it do
+      expect(subject.instance_variable_get(:@attrs).keys).to eq(arguments.keys)
+      expect(subject).to be_a(described_class)
+    end
+  end
+end
